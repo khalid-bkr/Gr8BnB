f1049205864e1cf905af772e96adce7dfce85f8e
khalid abubaker <khalid7.bkr@gmail.com>
2022-03-23 16:48:48 -0700

    Initial commit

---
+++ src/main/java/bnb/tools/Inserter.java
@@ -0,0 +1,22 @@
+package bnb.tools;
+
+
+import java.sql.SQLException;
+
+
+
+/**
+ * main() runner, used for the app demo.
+ * 
+ * Instructions:
+ * 1. Create a new MySQL schema and then run the CREATE TABLE statements from lecture:
+ * http://goo.gl/86a11H.
+ * 2. Update ConnectionManager with the correct user, password, and schema.
+ */
+public class Inserter {
+
+	public static void main(String[] args) throws SQLException {
+		// DAO instances.
+
+	}
+}
