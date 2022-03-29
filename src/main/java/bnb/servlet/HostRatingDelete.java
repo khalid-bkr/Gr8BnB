6f710fe362f7edcfcc839b2d38e1bd15f75866ed
Kelly Song <ksong11@uw.edu>
2022-03-29 15:14:13 -0700

    implement HostRatingDelete .java and .jsp

---
+++ src/main/java/bnb/servlet/HostRatingDelete.java
@@ -0,0 +1,68 @@
+package bnb.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bnb.dal.HostRatingDao;
+import bnb.model.Host;
+import bnb.model.HostRating;
+
+@WebServlet("/hostratingdelete")
+public class HostRatingDelete extends HttpServlet{
+	protected HostRatingDao hostRatingDao;
+	
+	@Override
+	public void init() throws ServletException {
+		hostRatingDao = HostRatingDao.getInstance();
+	}
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+        Map<String, String> messages = new HashMap<String, String>();
+        req.setAttribute("messages", messages);
+        messages.put("title", "Delete HostRating");        
+        req.getRequestDispatcher("/HostRatingDelete.jsp").forward(req, resp);
+	}
+	
+	@Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+    		throws ServletException, IOException {
+        Map<String, String> messages = new HashMap<String, String>();
+        req.setAttribute("messages", messages);
+        
+        String id = req.getParameter("id");
+        if (id == null || id.trim().isEmpty()) {
+            messages.put("title", "Invalid ID");
+            messages.put("disableSubmit", "true");
+        } else {
+        	int hostRatingId = Integer.parseInt(id);
+        	int hostId = Integer.parseInt(req.getParameter("hostid"));
+        	Host host = new Host(hostId);
+        	double rating = Double.parseDouble(req.getParameter("rating"));
+    		HostRating hostRating= new HostRating(hostRatingId, host, rating);
+        	try {
+        		hostRating = hostRatingDao.delete(hostRating);
+        		if (hostRating == null) {
+		            messages.put("title", "Successfully deleted " + id);
+		            messages.put("disableSubmit", "true");
+        		} else {
+		        	messages.put("title", "Failed to delete " + id);
+		        	messages.put("disableSubmit", "false");
+        		}
+        	} catch (SQLException e) {
+        		e.printStackTrace();
+        		throw new IOException(e);
+        	}
+        }
+        req.getRequestDispatcher("/HostRatingDelete.jsp").forward(req, resp);
+	}
+}
